050c5995fce8806948fdcbd2e26b79722d106763
Dakota Rennemann <rennemannd@ufl.edu>
2019-03-01 07:35:18 -0500

    WIP
    Basic filestructure stuff

---
+++ client/index.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+
+    <body>
+        <div>
+            <h3>Welcome to the Tempotatoes newest Project!</h3>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/1/1e/BlauerSchwede02.JPG" alt = "Potato image" height="1000" width="1000">
+        </div>
+    </body>
+
+</html>
+
+
