d7f8a57bd61e7155972f5f501bcf1671c1f03fdc
Dakota Rennemann <rennemannd@gmail.com>
2019-03-26 23:03:52 -0400

    Removed Tab component and made HeaderBox static

--- src/components/HeaderBox/HeaderBox.js
+++ src/components/HeaderBox/HeaderBox.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import Tabs from './Tabs.js';
+import { Link } from 'react-router-dom';
 
 // this component displays multiple tabs together
-const HeaderBox = ({pageTabs}) => {
+const HeaderBox = ( ) => {
     return (
-
         <div>
-            <img alt='Logo' src='https://mse.ufl.edu/wp-content/uploads/geufl2.jpg' width='400' height='200'/>
             {
-                pageTabs.map((user, i) => {
-                    return (
-                        <Tabs
-                            key={pageTabs[i].id}
-                            id={pageTabs[i].id}
-                            tabName={pageTabs[i].tabName}
-                            link={pageTabs[i].link}
-                        />
-                    );
+                <div>
+                    <img alt='Logo' src='https://mse.ufl.edu/wp-content/uploads/geufl2.jpg' width='400' height='200'/>
 
-                })
+                    <div className='bg-orange dib br3 pa3 ma2 grow bw2 shadow-5'>
+                        <Link to="/Home">Home</Link>
+                    </div>
+                    <div className='bg-orange dib br3 pa3 ma2 grow bw2 shadow-5'>
+                        <Link to="/Test">Test</Link>
+                    </div>
+                </div>
             }
         </div>
     );
