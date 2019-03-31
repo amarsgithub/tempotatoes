6465c87d20bf2d907672866976977e58dbaeda81
QuantumSoul <seanbiava@gmail.com>
2019-03-30 21:34:41 -0400

    Event calendar is rendered in the HTML.  Has a bunch of styling issues that need to be resolved.

--- client/src/views/Events/Events.js
+++ client/src/views/Events/Events.js
@@ -3,9 +3,18 @@ import React, { Component } from 'react';
 class Events extends Component {
     render() {
         return (
-            <div>
-                llllllooooolllll
-            </div>
+            <div className=".calendarStyle">
+            <iframe src="https://calendar.google.com/calendar/embed?src=seanbiava%40gmail.com&ctz=America%2FNew_York" 
+                          style={{
+                          align: 'center',
+                          border: '0', 
+                          width:"1800", 
+                          height: "1800", 
+                          frameborder: "0", 
+                          scrolling: "no"
+                          }}
+            ></iframe>
+        </div>
         )
     }
 }
