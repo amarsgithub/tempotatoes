3983e9fe6616fbe4045a47b9827db224797f6c97
QuantumSoul <seanbiava@gmail.com>
2019-03-30 21:23:43 -0400

    Added in the routing for the Events page

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import  Home  from './views/Home/Home';
 import Test  from './views/Test/Test';
+import Events from './views/Events/Events';
 import  Profile  from './views/Profile/Profile';
 import  NotFound  from './views/NotFound/NotFound';
 import { Route, Switch, Redirect } from 'react-router-dom';
@@ -19,6 +20,7 @@ export const Routes = () => {
                 </Route>
                 <Route exact path="/Test" component={Test} />
                 <Route path="/Home/:profileId" component={ Profile } />
+                <Route exact path="/Events" component={Events} />
                 <Route component={NotFound} />
             </Switch>
         </div>
