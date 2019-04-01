2a856e8baf65ddf8c07f4a819b6a6c7eac1b7f30
QuantumSoul <seanbiava@gmail.com>
2019-04-01 14:54:29 -0400

    Merging Events
    Made changes to header box too.  Going to start styling the page to look less like a fischer price toy

--- client/src/views/Events/Events.js
+++ client/src/views/Events/Events.js
@@ -4,16 +4,7 @@ class Events extends Component {
     render() {
         return (
             <div className=".calendarStyle">
-            <iframe src="https://calendar.google.com/calendar/embed?src=seanbiava%40gmail.com&ctz=America%2FNew_York" 
-                          style={{
-                          align: 'center',
-                          border: '0', 
-                          width:"1800", 
-                          height: "1800", 
-                          frameborder: "0", 
-                          scrolling: "no"
-                          }}
-            ></iframe>
+            <iframe src="https://calendar.google.com/calendar/embed?src=seanbiava%40gmail.com&ctz=America%2FNew_York"></iframe>
         </div>
         )
     }
