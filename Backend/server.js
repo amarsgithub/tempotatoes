3bcc9df75fef047a91ff2f18057a7c4bf6a54faf
Dakota Rennemann <rennemannd@gmail.com>
2019-04-21 11:01:38 -0400

    Remote testing

--- Backend/server.js
+++ Backend/server.js
@@ -34,7 +34,7 @@ app.use((err, req, res, next) => {
     next();
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
+const port =  5000;
+app.listen( port, () => {
     console.log(`Server running on port ${port}`)
 });
--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,4 +1,5 @@
 const proxy = require("http-proxy-middleware");
+require('dotenv').config();
 
 module.exports = app => {
     app.use(proxy("/api/*", { target: "http://localhost:5000/" }));
