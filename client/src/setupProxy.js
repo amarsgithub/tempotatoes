77ca1b9e111332f13bbf007a0972294ebf85186f
Dakota Rennemann <rennemannd@gmail.com>
2019-04-21 10:03:15 -0400

    I THINK ITS FIXED GOOD GOD

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,5 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = app => {
-    app.use(proxy("/api/*", { target: "https://tempotatoes.herokuapp.com/" }));
+    app.use(proxy("/api/*", { target: "http://localhost:5000/" }));
 };
