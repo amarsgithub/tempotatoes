f74b9fe2830b0411284eb9664545896a8c260a12
Sean Biava <seanbiava@gmail.com>
2019-04-22 11:50:36 -0400

    Began to fix styling format.  Added css files in view. Heirarchy works as follows: App.css contains all general styles: ex - text & headers all other styles will be made in the component or the view
    To-do: add header bar styling to the header bar folder.

--- client/src/views/Events/Events.js
+++ client/src/views/Events/Events.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Calendar from '../../components/Calendar/Calendar.js';
+import './Events.css';
+
 
 class Events extends Component {
     render() {
---
+++ src/views/Login/Login.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Login extends Component {
+    render(){
+        return(
+            <div>
+                This is the Login page
+            </div>
+        );
+    }
+}
+
+export default Login;
